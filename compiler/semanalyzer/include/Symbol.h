d8bcbae7c5e254aa1f04b217da62ae3dfc0de4f4
Afa Cheng <afa@afa.moe>
2024-09-09 18:57:23 +0800

    stage

--- compiler/semanalyzer/include/Symbol.h
+++ compiler/semanalyzer/include/Symbol.h
@@ -49,51 +49,9 @@ public:
 
     bool has_return() const { return _has_return; }
 
-    bool get_var_initialized() { return type == SymbolType::VARIABLE ? false : false; }
-
-    // [Group] Constant propagation functions
-    // If the variable is set with a constant and was not changed afterwards,
-    // the constant value could be propagated to the expression.
-    // If it's changed, or it's initialized as a non-constant, the value is invalid.
-
-    /**
-     * @brief Get the variable value of the symbol. The value may be invalid. Be sure to
-     * check get_var_value_valid().
-     *
-     * @return int
-     */
-    int get_var_value() { return type == SymbolType::VARIABLE ? _value : 0; }
-
-    /**
-     * @brief Set the variable value of the symbol. This will make the value valid.
-     *
-     * @param value
-     */
-    void set_var_value(int value)
-    {
-        _value = value;
-        _value_valid = true;
-    }
-
-    /**
-     * @brief Get if the variable value is valid.
-     *
-     * @return int
-     */
-    int get_var_value_valid() { return type == SymbolType::VARIABLE ? _value_valid : false; }
-
-    /**
-     * @brief Invalidate the variable value.
-     *
-     */
-    void invalidate_var_value() { _value_valid = false; }
-
 private:
     bool _has_param = false;
     bool _has_return = false;
-
-    int _value = 0;
-    bool _value_valid = false;
 };
 
 using SymbolPtr = std::shared_ptr<Symbol>;
