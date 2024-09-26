588ae7c042223714b5c38a7ebc03856b995c2dc0
Afa Cheng <afa@afa.moe>
2024-09-27 00:58:03 +0900

    Fix bug where if statement doesn't correctly handle var init results

--- compiler/semanalyzer/src/UseBeforeInitializationCheckPass.cpp
+++ compiler/semanalyzer/src/UseBeforeInitializationCheckPass.cpp
@@ -72,6 +72,8 @@ int UseBeforeInitializationCheckPass::visit(const parser::IfStatementASTNodePtr 
 {
     enter_node(node);
 
+    auto pre_if_result = get_var_init_at_current_scope();
+
     NodeResult then_result, else_result, merged_result;
     auto &then_branch = node->get_then_branch();
     if (then_branch) {
@@ -105,9 +107,19 @@ int UseBeforeInitializationCheckPass::visit(const parser::IfStatementASTNodePtr 
             value = 1;
         }
     }
-
     strip_symbols_beyond_scope(merged_result, get_current_scope_id());
-    set_var_init_at_current_scope(merged_result);
+
+    for (auto &[symbol, value] : pre_if_result) {
+        auto if_initialized_it = merged_result.find(symbol);
+
+        // if the if-block initialized this var, it's initialized anyway
+        // if it's not (either it's not seen in if-block -> not found, or it's 0), keep it as-is
+        if (if_initialized_it != merged_result.end() && if_initialized_it->second == 1) {
+            value = 1;
+        }
+    }
+
+    set_var_init_at_current_scope(pre_if_result);
 
     leave_node();
     return 0;
@@ -378,6 +390,19 @@ int UseBeforeInitializationCheckPass::get_var_init_at_current_scope(const Symbol
     auto &stack = _varinit_record_stacks[symbol];
     assert(!stack.empty());
     return stack.top();
+}
+
+UseBeforeInitializationCheckPass::NodeResult UseBeforeInitializationCheckPass::get_var_init_at_current_scope()
+{
+    const auto &current_scope = get_current_scope_id();
+    NodeResult result;
+    for (const auto &[sym, stack] : _varinit_record_stacks) {
+        if (_symbol_table->is_symbol_in_scope(sym, current_scope)) {
+            assert(!stack.empty());
+            result[sym] = stack.top();
+        }
+    }
+    return result;
 }
 
 void UseBeforeInitializationCheckPass::strip_symbols_beyond_scope(NodeResult &results, const std::string &scope_id)
