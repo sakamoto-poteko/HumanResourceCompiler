9780a7537743a211d5939a56f232777a048994f3
坂本ポテコ <afa@afa.moe>
2024-10-01 15:19:26 +0800

    IRGen: Add an SSA verifier pass to check SSA correctness (#81)

---
+++ compiler/irgen/include/VerifySSAPass.h
@@ -0,0 +1,49 @@
+#ifndef VERIFYSSAPASS_H
+#define VERIFYSSAPASS_H
+
+#include "IROptimizationPass.h"
+#include "IRProgramStructure.h"
+#include "ThreeAddressCode.h"
+#include "irgen_global.h"
+
+OPEN_IRGEN_NAMESPACE
+
+class VerifySSAPass : public IROptimizationPass {
+public:
+    VerifySSAPass(const ProgramPtr &program)
+        : IROptimizationPass(program)
+    {
+    }
+
+    ~VerifySSAPass() = default;
+
+protected:
+    int run_subroutine(const SubroutinePtr &subroutine, ProgramMetadata &metadata, const ProgramPtr &program) override;
+
+    template <typename VerifyFunc>
+    void visit_basic_block(ControlFlowVertex vertex, const ControlFlowGraph &cfg, std::set<ControlFlowVertex> &visited, VerifyFunc verify_func)
+    {
+        if (visited.contains(vertex)) {
+            return;
+        }
+        visited.insert(vertex);
+
+        const BasicBlockPtr &basic_block = cfg[vertex];
+
+        verify_func(basic_block);
+
+        for (ControlFlowEdge edge : boost::make_iterator_range(boost::out_edges(vertex, cfg))) {
+            ControlFlowVertex target = boost::target(edge, cfg);
+            visit_basic_block(target, cfg, visited, verify_func);
+        }
+    }
+
+    static void verify_basic_block_assignments_and_uses(const BasicBlockPtr &basic_block, std::set<unsigned int> &_variable_assigned);
+    static void verify_basic_block_phi_incoming_branches(const BasicBlockPtr &basic_block, const std::set<unsigned int> &_variable_assigned);
+
+private:
+};
+
+CLOSE_IRGEN_NAMESPACE
+
+#endif
